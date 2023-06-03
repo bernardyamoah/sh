4bd6146ab0d1a6672c79544dee8eb91d8a4df286
qweku2000 <94671690+qweku2000@users.noreply.github.com>
2023-06-03 22:11:20 +0000

    still problems with data retrieval in books

--- appwrite.js
+++ appwrite.js
@@ -1,46 +1,22 @@
-const { Client, Databases, Storage, Account } = require('appwrite');
+const { Client, Databases } = require('appwrite');
 const { v4: uuidv4 } = require('uuid');
 
-const client = new Client()
+const client = new Client();
 
-client.setEndpoint("https://cloud.appwrite.io/v1")
-client.setProject("647769963d8c335861a4")
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("647769963d8c335861a4");
 
+const database = new Databases(client, "64787ed6dfed97614a03");
 
- const database = new Databases(client,"64787ed6dfed97614a03")
+database.listDocuments("64787cd7520487b4dfb2", "64787ed6dfed97614a03")
+  .then(response => {
+    const documents = response.documents;
+    console.log('Documents:', documents);
+  })
+  .catch(error => {
+    console.log('Error retrieving documents:', error);
+  });
 
- 
-database.listDocuments("64787cd7520487b4dfb2","64787ed6dfed97614a03")
-.then(
-    response=>{
-        const documents = response.documents
-        console.log('Documents',documents)
-    }
-)
-.catch(
-    error=>{
-        console.log('Error retrieving documents',error)
-    }
-)
+const documentId = uuidv4().substring(0, 36); // Generate a UUID and truncate to maximum length
 
-database.createDocument("64787cd7520487b4dfb2",uuidv4(),
-{
-    "Title":"Callister Text Book"
-},
-
-
-{
-    "Title": "Callister Manual"
-})
-.then(
-    response=>
-    {
-        const document = response.$id
-        console.log('New Document',document)
-    })
-.catch (
-    error=>
-    {
-        console.error('Error creating document:',error)
-    }
-)
